5d4daec7ac6ce6c7ab1b837ad92a07b2f1ac6614
javimarashall <jamondragon@ucdavis.edu>
2021-03-24 19:07:54 -0700

    arrays

--- script.js
+++ script.js
@@ -11,12 +11,29 @@ var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialCharacters = ["~", "!", "@", "#", "$", "^", "&", "*"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var minCharacters = 8;
+var maxCharacters = 128;
+
+var indexUpper = Math.floor(Math.random() * upperCaseLetters.length);
+console.log(indexUpper)
+
+
 
 function  characterNumber() {
-  var response = prompt("Enter amount of characters between 8 and 128");
-  log(response);
+  var userInput = prompt("Enter amount of characters between 8 and 128");
+  if (userInput>= minCharacters && userInput <=maxCharacters) {
+    var useUpper = confirm("Would you like capital letters?");
+    var useLower = confirm("Would you like lower case letters?");
+    var numbers = confirm("Would you like to use numbers?");
+    var specialCharacters = confirm("Would you like special characters?")
+  } else {
+    alert("Password must be between 8 and 128 characters");
+  }
 }
-characterNumber()
+characterNumber();
+
+
+
 
 // Write password to the #password input
 function writePassword() {
